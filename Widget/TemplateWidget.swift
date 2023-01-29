dbfcf046453db44de494281862a9998a7afc738b
Imthath <imthath.m@hotmail.com>
2023-01-29 11:40:17 +0530

    Try setting up widget. Builds successfully but doesn't run simulator or device.

---
+++ Widget/TemplateWidget.swift
@@ -0,0 +1,66 @@
+//
+//  Template.swift
+//  Template
+//
+//  Created by Imthathullah on 28/01/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+struct TemplateEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .time)
+    }
+}
+
+struct TemplateWidget: Widget {
+    let kind: String = "Template"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            TemplateEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct Template_Previews: PreviewProvider {
+    static var previews: some View {
+        TemplateEntryView(entry: SimpleEntry(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
---
+++ Widget/TemplateWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TemplateBundle.swift
+//  Template
+//
+//  Created by Imthathullah on 28/01/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TemplateWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TemplateWidget()
+    }
+}
